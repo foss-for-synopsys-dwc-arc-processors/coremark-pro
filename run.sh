fdbbeb04db94f15a5c3a4ad786a0ce38dc7e2921
Claudiu Zissulescu <claziss@gmail.com>
2022-10-05 12:38:30 +0300

    Update expect script

--- run.sh
+++ run.sh
@@ -2,17 +2,13 @@
 
 set timeout 100
 
-set cpus { qemu_arc_hs6x}
+set cpus {qemu_arc_hs5x qemu_arc_hs6x}
 set bmrks {core cjpeg-rose7-preset parser-125k sha-test zip-test}
-set i 0
-set j 0
 
-while {$j < [llength $cpus]} {
-while {$i < [llength $bmrks]} {
-	  spawn west build -p -b [lindex $cpus $j] \
-		[lindex $bmrks $i] && west build -t run
-	  expect " Done:[lindex $bmrks $i]="
-	  incr i;
-      }
-      incr j
+for {set j 0} {$j < [llength $cpus]} {incr j} {
+    for {set i 0 } {$i < [llength $bmrks]} {incr i} {
+	spawn west build -p -b [lindex $cpus $j] \
+	    coremark-pro-zephyr/[lindex $bmrks $i] && west build -t run
+	expect " Done:[lindex $bmrks $i]="
+    }
 }
